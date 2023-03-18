1df57dc1929e267d868c5cc8d6c40d88c37c6485
João Matos <joaocarlosm00@gmail.com>
2023-03-18 02:02:09 +0000

    Reorganize src files

--- src/1_EDA.py
+++ src/1_EDA.py
@@ -4,7 +4,44 @@ from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.use('TKAgg')
 import seaborn as sns
+from tableone import TableOne
 
+# Table 1
+
+data = pd.read_csv('data/MIMIC_IV.csv')
+
+data['race_white'] = data.race_group == 'White'
+
+groupby = ['race_white']
+
+categorical = ['hidden_hypoxemia', 'gender', 'race_group', 'language',
+               'ventilation_status', 'invasive_vent', 'rrt',
+               'heart_rhythm']
+
+nonnormal = ['SaO2', 'delta_SpO2', 'SpO2', 'anchor_age', 'BMI',
+             'los_hospital', 'los_icu', 'CCI', 'SOFA_admission',
+             'sofa_coag', 'sofa_liver', 'sofa_cv',
+             'sofa_cns', 'sofa_renal', 'FiO2',
+             'norepinephrine_equivalent_dose', 
+             'hemoglobin', 'hematocrit', 'mch', 'mchc', 'mcv', 'platelet',
+             'rbc', 'rdw', 'wbc', 'd_dimer', 'fibrinogen',
+             'thrombin', 'inr', 'pt', 'ptt', 'alt', 'alp', 'ast',
+             'bilirubin_total', 'bilirubin_direct', 'bilirubin_indirect',
+             'ck_cpk', 'ck_mb', 'ggt', 'ld_ldh', 'albumin', 'aniongap',
+             'bicarbonate', 'bun', 'calcium', 'chloride', 'creatinine',
+             'glucose_lab', 'sodium', 'potassium', 'ph', 'lactate',
+             'heart_rate', 'mbp', 'resp_rate', 'temperature', 'glucose']
+
+# Create a TableOne 
+
+table1_raw = TableOne(data, columns=categorical+nonnormal,
+                      groupby=groupby, categorical=categorical, nonnormal=nonnormal,
+                      smd=True, 
+                      dip_test=True, normal_test=True, tukey_test=True, htest_name=True)
+
+table1_raw.to_excel('EDA/table1_rawW.xlsx')
+
+# Plots
 df = pd.read_csv('data/MIMIC_IV.csv')[['SaO2','SpO2','delta_SpO2', 'race_group']]
 df = df[(df.SaO2 <= 100) & (df.SpO2 <= 100) & (df.SaO2 >= 65) & (df.SpO2 >= 65)]
 df = df[df.delta_SpO2 >= -10]

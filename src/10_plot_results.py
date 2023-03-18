00aec552611c247ef9903f8b7a379d158dcf2832
João Matos <joaocarlosm00@gmail.com>
2023-03-18 01:51:48 +0000

    Add plot for the final XGBoost Results with SOFA resp

---
+++ src/10_plot_results.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TKAgg')
+
+model_name = "xgbr_wSOFA"
+
+# load the data
+data = pd.read_excel(f'results/{model_name}.xlsx', index_col=0)
+data = data.drop('Overall', axis=1)
+
+
+xx = np.arange(len(data.columns))
+
+# create the first subplot with baseline and model R²
+fig, axs = plt.subplots(1,2, figsize=(10, 4))
+axs[0].bar(xx-.3, data.loc['Baseline R²'] * 100,
+           width=.3, 
+           color='lightgray', label='Baseline')
+
+axs[0].bar(xx, data.loc['Model R²'] * 100,
+           width=.3, #alpha=.8,
+           color='mediumseagreen', label='Model')
+
+axs[0].set_title('SaO2 vs. SpO2 Goodness of Fit')
+axs[0].set_xticks(xx, data.columns)
+axs[0].set_xlabel("Race Group")
+axs[0].set_ylabel('R² (%)')
+axs[0].set_ylim([-100, 100])
+axs[0].axhline(y=0, color='k', linewidth=.7)
+
+# create the second subplot with baseline and model HH
+axs[1].bar(xx-.3, data.loc['Baseline HH'] / data.loc['N'] * 100,
+           width=.3,
+           color='lightgray', label='Baseline')
+
+axs[1].bar(xx, data.loc['Model HH'] / data.loc['N'] * 100,
+           width=.3,
+           color='mediumseagreen', label='Model')
+
+axs[1].set_title('Presence of Hidden Hypoxemias, H.H.')
+axs[1].set_xticks(xx, data.columns)
+axs[1].set_xlabel("Race Group")
+axs[1].set_ylabel('H.H. (%)')
+axs[1].set_ylim([0, 4])
+plt.xticks(xx, data.columns)
+axs[1].legend(loc='upper left', bbox_to_anchor=(1.05, .6))
+
+# add a title and save the figure
+plt.suptitle('SaO2 - SpO2 Correction Model', fontsize=16)
+plt.tight_layout()
+plt.savefig(f'results/{model_name}.png', dpi=300)
